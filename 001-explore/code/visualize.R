0f9248d1989df8b3628b5d1a277eb34745ec5eac
paradisepilot <paradisepilot@gmail.com>
2020-01-06 21:09:14 -0500

    updated: visualize.R

--- 001-explore/code/visualize.R
+++ 001-explore/code/visualize.R
@@ -25,14 +25,16 @@ visualize <- function(
     cat("\nsummary(DF.temp)\n");
     print( summary(DF.temp)   );
 
+    saveRDS(object = DF.temp, file = "temp.RData");    
+
     visualize_scatter3D(
         DF.input = DF.temp[DF.temp[,"type"] == "marsh",],
-	PNG.output = 'tmp-scatter3D-marsh.png'
+	    PNG.output = 'tmp-scatter3D-marsh.png'
         );
 
     visualize_scatter3D(
         DF.input = DF.temp[DF.temp[,"type"] == "swamp",],
-	PNG.output = 'tmp-scatter3D-swamp.png'
+	    PNG.output = 'tmp-scatter3D-swamp.png'
         );
 
     ### ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ###
@@ -43,10 +45,42 @@ visualize <- function(
     }
 
 ###################################################
+visualize_plotly_scatter3d <- function(
+    DF.input   = NULL,
+    PNG.output = 'tmp-scatter3D.png'
+    ) {
+    plotly::plot_ly(
+        x       = DF.temp[,"Band_1"],
+        y       = DF.temp[,"Band_2"],
+        z       = DF.temp[,"Band_3"],
+        type    = "scatter3d",
+        mode    = "markers",
+        size    = 0.75,
+        color   = DF.temp[,"plot.colour"],
+        colors  = c("red","blue"),
+        opacity = 0.3
+        );
+    }
+
 visualize_scatter3D <- function(
     DF.input   = NULL,
     PNG.output = 'tmp-scatter3D.png'
     ) {
+    #require(plotly);
+    #my.plot <- plotly::plot_ly(
+    #    x      = DF.input[,"Band_1"],
+    #    y      = DF.input[,"Band_2"],
+    #    z      = DF.input[,"Band_3"],
+    #    type   = "scatter3d",
+    #    mode   = "markers"#,
+    #    #color = as.integer(DF.input[,"type"])
+    #    #width  = NULL,
+    #    #height = NULL
+    #    );
+    #plotly::orca(
+    #    p    = my.plot,
+    #    file = PNG.output
+    #    );
     require(plot3D);
     png(filename = PNG.output, height = 6, width = 6, units = "in", res = 300);
     plot3D::scatter3D(
