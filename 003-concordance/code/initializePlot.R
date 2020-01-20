932704c5dda3b27114648c1be7f779a947290bdb
paradisepilot <paradisepilot@gmail.com>
2020-01-20 00:41:10 -0500

    added: 003-concordance/

---
+++ 003-concordance/code/initializePlot.R
@@ -0,0 +1,33 @@
+
+initializePlot <- function(
+    textsize.title = 30,
+    textsize.axis  = 20,
+    title          = 'my-title',
+    subtitle       = 'my-subtitle'
+    ) {
+
+    require(ggplot2);
+
+    # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ #
+    my.ggplot <- ggplot(data = NULL) + theme_bw();
+    my.ggplot <- my.ggplot + theme(
+        title            = element_text(size = textsize.title, face = "bold"),
+        axis.title.x     = element_blank(),
+        axis.title.y     = element_blank(),
+        axis.text.x      = element_text(size = textsize.axis,  face = "bold"),
+        axis.text.y      = element_text(size = textsize.axis,  face = "bold"),
+        legend.text      = element_text(size = textsize.axis),
+        panel.grid.major = element_line(colour="gray", linetype=2, size=0.25),
+        panel.grid.minor = element_line(colour="gray", linetype=2, size=0.25)
+        );
+
+    my.ggplot <- my.ggplot + labs(
+        title    = title,
+        subtitle = subtitle
+        );
+
+    # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ #
+    return( my.ggplot );
+
+    }
+
---
+++ 003-concordance/code/runall.R
@@ -0,0 +1,114 @@
+
+command.arguments <- commandArgs(trailingOnly = TRUE);
+data.directory    <- normalizePath(command.arguments[1]);
+code.directory    <- normalizePath(command.arguments[2]);
+output.directory  <- normalizePath(command.arguments[3]);
+
+# add custom library using .libPaths()
+print( data.directory   );
+print( code.directory   );
+print( output.directory );
+print( format(Sys.time(),"%Y-%m-%d %T %Z") );
+
+start.proc.time <- proc.time();
+
+# set working directory to output directory
+setwd( output.directory );
+
+##################################################
+# source supporting R code
+
+code.files <- c(
+    "initializePlot.R"
+    ); 
+
+for ( code.file in code.files ) {
+    source(file.path(code.directory,code.file));
+    }
+
+### ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ###
+set.seed(7654321);
+
+DF.concordance <- readr::read_csv(
+    file = file.path(data.directory,"ken_fixed_excel.csv")
+    );
+
+print( str(DF.concordance) );
+
+print( summary(DF.concordance) );
+
+### ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ###
+cor(x = DF.concordance$test,       y = DF.concordance$reference,       method="pearson");
+
+cor(x = DF.concordance$test,       y = DF.concordance$reference,       method="spearman");
+
+cor(x = rank(DF.concordance$test), y = rank(DF.concordance$reference), method="pearson");
+
+### ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ###
+DF.temp <- DF.concordance + matrix(
+    rnorm(2 * nrow(DF.concordance), mean = 0, sd = 0.1),
+    nrow = nrow(DF.concordance)
+    );
+
+print( str(DF.temp) );
+
+### ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ###
+PNG.output = 'tmp-pearson.png'
+
+my.ggplot <- initializePlot(title = NULL, subtitle = NULL);
+my.ggplot <- my.ggplot + theme(legend.position = "none");
+
+my.ggplot <- my.ggplot + geom_point(
+    data    = DF.temp,
+    mapping = aes(x = reference, y = test, colour = "red"),
+    size    = 0.1,
+    alpha   = 0.1
+    );
+
+ggsave(
+    file   = PNG.output,
+    plot   = my.ggplot,
+    dpi    = 300,
+    height =   8,
+    width  =   8,
+    units  = 'in'
+    );
+
+### ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ###
+PNG.output = 'tmp-spearman.png'
+
+my.ggplot <- initializePlot(title = NULL, subtitle = NULL);
+my.ggplot <- my.ggplot + theme(legend.position = "none");
+
+my.ggplot <- my.ggplot + geom_point(
+    data    = DF.temp,
+    mapping = aes(x = rank(reference), y = rank(test), colour = "red"),
+    size    = 0.1,
+    alpha   = 0.1
+    );
+
+ggsave(
+    file   = PNG.output,
+    plot   = my.ggplot,
+    dpi    = 300,
+    height =   8,
+    width  =   8,
+    units  = 'in'
+    );
+
+### ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ###
+
+##################################################
+print( warnings() );
+
+print( getOption('repos') );
+
+print( .libPaths() );
+
+print( sessionInfo() );
+
+print( format(Sys.time(),"%Y-%m-%d %T %Z") );
+
+stop.proc.time <- proc.time();
+print( stop.proc.time - start.proc.time );
+
