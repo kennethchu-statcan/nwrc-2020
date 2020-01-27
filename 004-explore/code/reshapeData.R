f7ca9d700ab99847607190e4292a9e62628e9c01
kennethchu-statcan <kenneth.chu@canada.ca>
2020-01-27 14:14:29 -0500

    updated: reshapeData.R

--- 004-explore/code/reshapeData.R
+++ 004-explore/code/reshapeData.R
@@ -54,10 +54,6 @@ reshapeData_long <- function(
     require(dplyr);
     require(tidyr);
 
-    print("A-1");
-    cat("\nstr(DF.input)\n");
-    print( str(DF.input)   );
-
     colnames(DF.input) <- gsub(
 	x           = colnames(DF.input),
         pattern     = "X2",
@@ -76,92 +72,30 @@ reshapeData_long <- function(
         value   = TRUE
         );
 
-    print("A-2");
-    cat("\ntemp.colnames\n");
-    print( temp.colnames   );
-
     DF.temp <- DF.input[,c("X","Y",temp.colnames)];
-
-    print("A-3");
-    cat("\nstr(DF.temp)\n");
-    print( str(DF.temp)   );
-    cat("\nsummary(DF.temp)\n");
-    print( summary(DF.temp)   );
-    cat("\nhead(DF.temp)\n");
-    print( head(DF.temp)   );
 
     DF.temp <- DF.temp %>%
         tidyr::gather(column.name,value,-X,-Y);
 
-    print("A-4");
-    cat("\nstr(DF.temp)\n");
-    print( str(DF.temp)   );
-    cat("\nsummary(DF.temp)\n");
-    print( summary(DF.temp)   );
-    cat("\nhead(DF.temp)\n");
-    print( head(DF.temp)   );
-
     DF.temp <- as.data.frame(DF.temp);
-
-    print("A-5");
-    cat("\nstr(DF.temp)\n");
-    print( str(DF.temp)   );
-    cat("\nsummary(DF.temp)\n");
-    print( summary(DF.temp)   );
-    cat("\nhead(DF.temp)\n");
-    print( head(DF.temp)   );
 
     DF.temp[,"date"] <- stringr::str_extract(
         string  = DF.temp[,"column.name"],
 	pattern = "[0-9]{8}"
 	);
 
-    print("A-6");
-    cat("\nstr(DF.temp)\n");
-    print( str(DF.temp)   );
-    cat("\nsummary(DF.temp)\n");
-    print( summary(DF.temp)   );
-    cat("\nhead(DF.temp)\n");
-    print( head(DF.temp)   );
-
     DF.temp[,"variable"] <- stringr::str_extract(
         string  = DF.temp[,"column.name"],
 	pattern = paste0(colname.pattern,".*")
 	);
 
-    print("A-7");
-    cat("\nstr(DF.temp)\n");
-    print( str(DF.temp)   );
-    cat("\nsummary(DF.temp)\n");
-    print( summary(DF.temp)   );
-    cat("\nhead(DF.temp)\n");
-    print( head(DF.temp)   );
-
     DF.temp <- DF.temp[,c("X","Y","date","variable","value")];
-
-    print("A-8");
-    cat("\nstr(DF.temp)\n");
-    print( str(DF.temp)   );
-    cat("\nsummary(DF.temp)\n");
-    print( summary(DF.temp)   );
-    cat("\nhead(DF.temp)\n");
-    print( head(DF.temp)   );
 
     DF.output <- DF.temp %>%
         dplyr::select(X,Y,date,variable,value) %>%
         tidyr::spread(key=variable,value=value);
 
-    print("A-9");
-    cat("\nstr(DF.temp)\n");
-    print( str(DF.temp)   );
-    cat("\nsummary(DF.temp)\n");
-    print( summary(DF.temp)   );
-    cat("\nhead(DF.temp)\n");
-    print( head(DF.temp)   );
-
     DF.output[,"date"] <- as.Date(x = DF.output[,"date"], tryFormats = c("%Y%m%d"));
-
-    print("A-10");
 
     return(DF.output);
 
