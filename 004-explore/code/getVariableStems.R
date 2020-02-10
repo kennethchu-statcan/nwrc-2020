a793bf67ed0c412d4c57629ac3ce652e0dbc8b5f
kennethchu-statcan <kenneth.chu@canada.ca>
2020-02-10 16:37:05 -0500

    updated: getVariableStems.R

--- 004-explore/code/getVariableStems.R
+++ 004-explore/code/getVariableStems.R
@@ -8,19 +8,19 @@ getVariableStems <- function() {
 
     ### ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ###
     list.output <- list(
-#       polarization     = c("cov_matrix_real_comp"),
-#       intensity_ratio  = c("intensity_ratio"),
-#       FreeDur          = c("FreeDur"),
-#       yamaguchi        = c("Yamaguchi"),
-#       entropy          = c("entropy_shannon"),
-        HH_VV            = c("HH_VV_"),
-	CloudPott        = c("CloudPott"),
-	Touz             = c("Touz_"),
-	TouzDisc         = c("TouzDisc"),
-        CircBasisChange  = c("CircBasisChange"),
-	total_power      = c("total_power"),
-	pedestal_height  = c("pedestal_height"),
-	phase_diff_HH_VV = c("phase_difference_HH_VV")
+#       polarization           = c("cov_matrix_real_comp"),
+#       intensity_ratio        = c("intensity_ratio"),
+#       FreeDur                = c("FreeDur"),
+#       yamaguchi              = c("Yamaguchi"),
+#       entropy                = c("entropy_shannon"),
+#       HH_VV                  = c("HH_VV_"),
+	CloudPott              = c("CloudPott"),
+	Touz                   = c("Touz_"),
+	TouzDisc               = c("TouzDisc"),
+        CircBasisChange        = c("CircBasisChange"),
+	total_power            = c("total_power"),
+	pedestal_height        = c("pedestal_height"),
+	phase_difference_HH_VV = c("phase_difference_HH_VV")
         );
 
     ### ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ###
