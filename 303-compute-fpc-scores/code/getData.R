1e62adb0e24cd5293f2fadb2de1b71b8fa96bac2
Kenneth Chu <kennethchu@Kenneths-MacBook-Pro.local>
2020-11-07 15:38:55 -0500

    updated: getData.R main.R

--- 303-compute-fpc-scores/code/getData.R
+++ 303-compute-fpc-scores/code/getData.R
@@ -23,36 +23,17 @@ getData <- function(
 
         temp.files <- list.files(path = data.directory);
 
-        cat("\ntemp.files\n");
-        print( temp.files   );
-
         DF.metadata <- getData_metadata(
             data.files = temp.files
             );
 
-        cat("\nstr(DF.metadata)\n");
-        print( str(DF.metadata)   );
-
-        cat("\nDF.metadata\n");
-        print( DF.metadata   );
-
-        cat("\nunique(DF.metadata[,'date'])\n");
-        print( unique(DF.metadata[,'date'])   );
-
-        cat("\nunique(DF.metadata[,'date'])[1:2]\n");
-        print( unique(DF.metadata[,'date'])[1:2]   );
-
         DF.output <- NULL;
         for ( temp.string in unique(as.character(DF.metadata[,'date'])) ) {
-            cat("\ntemp.string\n");
-            print( temp.string   );
             DF.given.date <- getData_given.date(
                 given.date     = as.Date(temp.string),
                 DF.metadata    = DF.metadata,
                 data.directory = data.directory
                 );
-            cat("\nstr(DF.given.date)\n");
-            print( str(DF.given.date)   );
             if ( is.null(DF.output) ) {
                 DF.output <- DF.given.date;
             } else {
@@ -79,16 +60,10 @@ getData_given.date <- function(
     DF.metadata    = NULL,
     data.directory = NULL
     ) {
-    cat("\ngiven.date\n");
-    print( given.date   );
     DF.metadata <- DF.metadata[DF.metadata[,'date'] == given.date,];
     DF.output <- NULL;
     for ( i in seq(1,nrow(DF.metadata))) {
-        cat("\ndata.file\n");
-        print( file.path(data.directory,DF.metadata[i,'data.file']) );
         DF.temp   <- read.csv(file = file.path(data.directory,DF.metadata[i,'data.file']), header = FALSE);
-        cat("\nis.null(DF.output)\n");
-        print( is.null(DF.output)   );
         if ( is.null(DF.output) ) {
             DF.output <- data.frame(temp_colname = as.vector(as.matrix(DF.temp)));
         } else {
@@ -96,11 +71,7 @@ getData_given.date <- function(
             }
         DF.output <- as.data.frame(DF.output);
         colnames(DF.output) <- gsub(x = colnames(DF.output), pattern = 'temp_colname', replacement = DF.metadata[i,'variable']);
-        cat("\nstr(DF.output)\n");
-        print( str(DF.output)   );
         }
-    cat("\ngiven.date\n");
-    print( given.date   );
     DF.output[,'date'] <- given.date;
     DF.output <- DF.output[,c('date',setdiff(colnames(DF.output),'date'))];
     return( DF.output );
@@ -122,259 +93,4 @@ getData_metadata <- function(
     DF.output[,'data.file'] <- data.files;
 
     return( DF.output );
-    }
-
-getData_add.dBZ.variables <- function(DF.input = NULL) {
-    DF.output <- DF.input;
-    temp.colnames <- grep(x = colnames(DF.output), pattern = "cov_matrix_real_comp", value = TRUE);
-    for ( temp.colname in temp.colnames ) {
-        new.colname <- paste0(temp.colname,"_dBZ");
-        DF.output[,new.colname] <- 10 * log10(DF.output[,temp.colname]);
-        }
-    colnames(DF.output) <- gsub(
-        x           = colnames(DF.output),
-        pattern     = "cov_matrix_real_comp_1_dBZ",
-        replacement = "dBZ_cov_comp_1"
-        );
-    colnames(DF.output) <- gsub(
-        x           = colnames(DF.output),
-        pattern     = "cov_matrix_real_comp_2_dBZ",
-        replacement = "dBZ_cov_comp_2"
-        );
-    colnames(DF.output) <- gsub(
-        x           = colnames(DF.output),
-        pattern     = "cov_matrix_real_comp_3_dBZ",
-        replacement = "dBZ_cov_comp_3"
-        );
-    colnames(DF.output) <- gsub(
-        x           = colnames(DF.output),
-        pattern     = "cov_matrix_real_comp_4_dBZ",
-        replacement = "dBZ_cov_comp_4"
-        );
-    return(DF.output);
-    }
-
-getData_fixColnames_Sentinel <- function(input.colnames = NULL) {
-
-    thisFunctionName <- "getData_fixColnames_Sentinel";
-
-    cat("\n### ~~~~~~~~~~~~~~~~~~~~ ###");
-    cat(paste0("\n",thisFunctionName,"() starts.\n\n"));
-
-    output.colnames <- input.colnames;
-
-    output.colnames <- gsub(
-        x           = output.colnames,
-        pattern     = "POINT_X",
-        replacement = "X"
-        );
-
-    output.colnames <- gsub(
-        x           = output.colnames,
-        pattern     = "POINT_Y",
-        replacement = "Y"
-        );
-
-    output.colnames <- gsub(
-        x           = output.colnames,
-        pattern     = "X2",
-        replacement = "X"
-        );
-
-    output.colnames <- gsub(
-        x           = output.colnames,
-        pattern     = "X3",
-        replacement = "Y"
-        );
-
-    output.colnames <- gsub(
-        x           = output.colnames,
-        pattern     = "_1_1",
-        replacement = "_1"
-        );
-
-    output.colnames <- gsub(
-        x           = output.colnames,
-        pattern     = "_2_2",
-        replacement = "_2"
-        );
-
-    output.colnames <- gsub(
-        x           = output.colnames,
-        pattern     = "_3_3",
-        replacement = "_3"
-        );
-
-    output.colnames <- gsub(
-        x           = output.colnames,
-        pattern     = "_4_4",
-        replacement = "_4"
-        );
-
-    output.colnames <- gsub(
-        x           = output.colnames,
-        pattern     = "Yamaguchi_double_bounc$",
-        replacement = "Yamaguchi_double_bounce"
-        );
-
-    output.colnames <- gsub(
-        x           = output.colnames,
-        pattern     = "_re$",
-        replacement = "_real"
-        );
-
-    output.colnames <- gsub(
-        x           = output.colnames,
-        pattern     = "_im$",
-        replacement = "_imag"
-        );
-
-    output.colnames <- gsub(
-        x           = output.colnames,
-        pattern     = "S_par$",
-        replacement = "S_param"
-        );
-
-    output.colnames <- gsub(
-        x           = output.colnames,
-        pattern     = "Helicit$",
-        replacement = "Helicity"
-        );
-
-    output.colnames <- gsub(
-        x           = output.colnames,
-        pattern     = "TouzDisc_diff_maxmin_r$",
-        replacement = "TouzDisc_diff_maxmin_res"
-        );
-
-    output.colnames <- gsub(
-        x           = output.colnames,
-        pattern     = "_pol_respo$",
-        replacement = "_pol_respons"
-        );
-
-    # standardize order of components in column names
-    output.colnames <- as.character(sapply(
-        X   = output.colnames,
-        FUN = function(x) {
-            y <- unlist(strsplit(x = x, split = "_"));
-            if ( length(y) > 1 ) {
-                y <- c(y[1],paste0(y[2],y[5]),paste0(y[3],y[4]),y[6]);
-                y <- paste(y,collapse = "_");
-                }
-            return(y);
-            }
-        ));
-
-    ### ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ###
-    cat(paste0("\n",thisFunctionName,"() quits."));
-    cat("\n### ~~~~~~~~~~~~~~~~~~~~ ###\n");
-    return( output.colnames );
-
-    }
-
-getData_fixColnames_RADARSAT <- function(input.colnames = NULL) {
-
-    thisFunctionName <- "getData_fixColnames_RADARSAT";
-
-    cat("\n### ~~~~~~~~~~~~~~~~~~~~ ###");
-    cat(paste0("\n",thisFunctionName,"() starts.\n\n"));
-
-    output.colnames <- input.colnames;
-
-    output.colnames <- gsub(
-        x           = output.colnames,
-        pattern     = "POINT_X",
-        replacement = "X"
-        );
-
-    output.colnames <- gsub(
-        x           = output.colnames,
-        pattern     = "POINT_Y",
-        replacement = "Y"
-        );
-
-    output.colnames <- gsub(
-        x           = output.colnames,
-        pattern     = "X2",
-        replacement = "X"
-        );
-
-    output.colnames <- gsub(
-        x           = output.colnames,
-        pattern     = "X3",
-        replacement = "Y"
-        );
-
-    output.colnames <- gsub(
-        x           = output.colnames,
-        pattern     = "_1_1",
-        replacement = "_1"
-        );
-
-    output.colnames <- gsub(
-        x           = output.colnames,
-        pattern     = "_2_2",
-        replacement = "_2"
-        );
-
-    output.colnames <- gsub(
-        x           = output.colnames,
-        pattern     = "_3_3",
-        replacement = "_3"
-        );
-
-    output.colnames <- gsub(
-        x           = output.colnames,
-        pattern     = "_4_4",
-        replacement = "_4"
-        );
-
-    output.colnames <- gsub(
-        x           = output.colnames,
-        pattern     = "Yamaguchi_double_bounc$",
-        replacement = "Yamaguchi_double_bounce"
-        );
-
-    output.colnames <- gsub(
-        x           = output.colnames,
-        pattern     = "_re$",
-        replacement = "_real"
-        );
-
-    output.colnames <- gsub(
-        x           = output.colnames,
-        pattern     = "_im$",
-        replacement = "_imag"
-        );
-
-    output.colnames <- gsub(
-        x           = output.colnames,
-        pattern     = "S_par$",
-        replacement = "S_param"
-        );
-
-    output.colnames <- gsub(
-        x           = output.colnames,
-        pattern     = "Helicit$",
-        replacement = "Helicity"
-        );
-
-    output.colnames <- gsub(
-        x           = output.colnames,
-        pattern     = "TouzDisc_diff_maxmin_r$",
-        replacement = "TouzDisc_diff_maxmin_res"
-        );
-
-    output.colnames <- gsub(
-        x           = output.colnames,
-        pattern     = "_pol_respo$",
-        replacement = "_pol_respons"
-        );
-
-    ### ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ###
-    cat(paste0("\n",thisFunctionName,"() quits."));
-    cat("\n### ~~~~~~~~~~~~~~~~~~~~ ###\n");
-    return( output.colnames );
-
     }
--- 303-compute-fpc-scores/code/main.R
+++ 303-compute-fpc-scores/code/main.R
@@ -23,6 +23,8 @@ require(rlang);
 ### ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ###
 code.files <- c(
     "getData.R",
+    "getData-beam-mode.R",
+    "getData-beam-mode-helper.R",
     "reshapeData.R"
     );
 
@@ -41,23 +43,170 @@ for ( pkg.file in pkg.files ) {
     }
 
 ### ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ###
-data.snapshot  <- "2020-10-13.01";
-data.directory <- file.path(dir.data,data.snapshot);
-
-### ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ###
 set.seed(7654321);
 
+DF.colour.scheme <- data.frame(
+    land.cover = c("marsh",  "swamp",  "water",  "forest", "ag",     "shallow"),
+    colour     = c("#000000","#E69F00","#56B4E9","#009E73","#F0E442","red"    )
+    );
+rownames(DF.colour.scheme) <- DF.colour.scheme[,"land.cover"];
+
 ### ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ###
-years <- c("2017","2018","2019");
+satellites      <- "sentinel";
+beam.mode       <- "IW4";
+colname.pattern <- "V";
+
+data.snapshot       <- "2020-02-24.03";
+data.directory      <- file.path(dir.data,data.snapshot,"Sentinal1","relative-orbit-number");
+beam.mode.directory <- file.path(data.directory,beam.mode);
+
+DF.IW4 <- getData.beam.mode(
+    data.directory  = beam.mode.directory,
+    satellites      = satellites,
+    beam.mode       = beam.mode,
+    colname.pattern = colname.pattern,
+    land.cover      = DF.colour.scheme[,'land.cover'],
+    RData.output    = paste0("data-labelled-",beam.mode,".RData")
+    );
+
+is.not.2016 <- ("2016" != DF.IW4[,'year']);
+DF.IW4 <- DF.IW4[is.not.2016,c(c("X","Y","date","type","VV","VH"))];
+colnames(DF.IW4) <- gsub(x = colnames(DF.IW4), pattern = "^X$",    replacement = "x");
+colnames(DF.IW4) <- gsub(x = colnames(DF.IW4), pattern = "^Y$",    replacement = "y");
+colnames(DF.IW4) <- gsub(x = colnames(DF.IW4), pattern = "^type$", replacement = "land_cover");
+
+cat("\nstr(DF.IW4)\n");
+print( str(DF.IW4)   );
+
+DF.VV <- DF.IW4[,c("x","y","date","VV")];
+DF.VV[,"x_y"] <- apply(
+    X      = DF.VV[,c('x','y')],
+    MARGIN = 1,
+    FUN    = function(x) { return(paste(x,collapse="_")) }
+    );
+
+cat("\nstr(DF.VV)\n");
+print( str(DF.VV)   );
+
+### ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ###
+#logger::log_threshold(level = logger::ERROR);
+logger::log_threshold(level = logger::TRACE);
+
+n.partition         <- 100;
+n.order             <-   3;
+n.basis             <-   9;
+smoothing.parameter <-   0.1;
+n.harmonics         <-   7;
+
+my.fpcFeatureEngine <- fpcFeatureEngine$new(
+    training.data       = DF.VV,
+    location            = 'x_y',
+    date                = 'date',
+    variable            = 'VV',
+    n.partition         = n.partition,
+    n.order             = n.order,
+    n.basis             = n.basis,
+    smoothing.parameter = smoothing.parameter,
+    n.harmonics         = n.harmonics
+    );
+
+my.fpcFeatureEngine$fit();
+
+ggplot2::ggsave(
+    file   = "plot-harmonics.png",
+    plot   = my.fpcFeatureEngine$plot.harmonics(),
+    dpi    = 150,
+    height =   4 * n.harmonics,
+    width  =  16,
+    units  = 'in'
+    );
+
+### ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ###
+data.snapshot  <- "2020-10-13.01";
+data.directory <- file.path(dir.data,data.snapshot);
+years          <- c("2017","2018","2019");
+n.batches      <- 500;
+
 for ( temp.year in years ) {
+
     DF.temp.year <- getData(
         data.directory = file.path(data.directory,temp.year),
-        output.file    = paste0("raw-",temp.year,".RData")
+        output.file    = paste0("data-unlabelled-",temp.year,".RData")
         );
-    cat("\nstr(DF.temp.year)\n");
-    print( str(DF.temp.year)   );
-    remove( list = "DF.temp.year" );
+
+    DF.lat.lon <- unique(DF.temp.year[,c('lat','lon')]);
+
+    DF.temp.year[,"batch"] <- sample(
+        x       = seq(1,n.batches,1),
+        size    = nrow(DF.temp.year),
+        replace = TRUE
+        );
+
+    for ( temp.batch in seq(1,n.batches,1) ) {
+
+        DF.temp.batch <- DF.temp.year[temp.batch == DF.temp.year[,'batch'],];
+
+        DF.temp.batch[,"lat_lon"] <- apply(
+            X      = DF.temp.batch[,c('lat','lon')],
+            MARGIN = 1,
+            FUN    = function(x) { return(paste(x,collapse="_")) }
+            );
+
+        cat(paste0("\nstr(DF.temp.batch) -- ",temp.year," -- ",temp.batch,"\n"));
+        print( str(DF.temp.batch)   );
+
+        DF.bspline.fpc <- my.fpcFeatureEngine$transform(
+            newdata  = DF.temp.batch,
+            location = 'lat_lon',
+            date     = 'date',
+            variable = 'vv'
+            );
+
+        cat("\nstr(DF.bspline.fpc)\n");
+        print( str(DF.bspline.fpc)   );
+
+        selected.colnames <- grep(
+            x       = colnames(DF.bspline.fpc),
+            pattern = "^[0-9]+$",
+            invert  = TRUE,
+            value   = TRUE
+            );
+
+        DF.fpc <- DF.bspline.fpc[,selected.colnames];
+
+        cat("\nstr(DF.fpc)\n");
+        print( str(DF.fpc)   );
+
+        temp.batch.string <- stringr::str_pad(
+            string = as.character(temp.batch),
+            width  = 1 + base::floor(base::log10(n.batches)),
+            pad    = "0"
+            );
+
+        saveRDS(
+            object = DF.fpc,
+            file   = paste0("fpc-unlabelled-",temp.year,"-batch-",temp.batch.string,".RData")
+            );
+
+        # png('plot-scatter-fpc1-fpc2.png', height = 8, width = 8, unit = 'in', res = 300);
+        # plot(
+        #     x    = DF.bspline.fpc[,'fpc_1'],
+        #     y    = DF.bspline.fpc[,'fpc_2'],
+        #     pch  = 19,
+        #     cex  =  0.1,
+        #     xlim = 300 * c(-1,1),
+        #     ylim = 150 * c(-1,1)
+        #     );
+        # dev.off()
+
+        remove( list = c("DF.bspline.fpc","DF.fpc") );
+
+        }
+
+    remove( list = c("DF.temp.year") );
+
     }
+
 
 ### ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ###
 
