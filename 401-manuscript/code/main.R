d65a64a597b383e52ad4df8232179dcf0287f4e0
Kenneth Chu <kennethchu@Kenneths-MacBook-Pro.local>
2020-12-29 15:39:30 -0500

    updated: main.R

--- 401-manuscript/code/main.R
+++ 401-manuscript/code/main.R
@@ -340,11 +340,11 @@ for ( temp.variable in c("VH","VV") ) {
             #     );
             # dev.off()
 
-            remove( list = c("DF.temp.batch","DF.bspline.fpc","DF.fpc") );
+            # remove( list = c("DF.temp.batch","DF.bspline.fpc","DF.fpc") );
 
             }
 
-        remove( list = c("DF.temp.year","my.fpcFeatureEngine") );
+        # remove( list = c("DF.temp.year","my.fpcFeatureEngine") );
 
         }
 
