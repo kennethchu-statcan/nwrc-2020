6b217ac2e594214b1fc880f5bb3d4d8c6f44108e
Kenneth Chu <kennethchu@Kenneths-MacBook-Pro.local>
2020-12-30 16:46:39 -0500

    updated: run-main.sh

--- 401-manuscript/run-main.sh
+++ 401-manuscript/run-main.sh
@@ -24,24 +24,28 @@ R --no-save --args ${dataDIR} ${codeDIR} ${pkgDIR} ${outputDIR} < ${myRscript} >
 sleep 30
 
 ### ~~~~~~~~~~ ###
-myRscript=${codeDIR}/main-02.R
-stdoutFile=${outputDIR}/stdout.R.`basename ${myRscript} .R`
-stderrFile=${outputDIR}/stderr.R.`basename ${myRscript} .R`
-R --no-save --args ${dataDIR} ${codeDIR} ${pkgDIR} ${outputDIR} < ${myRscript} > ${stdoutFile} 2> ${stderrFile}
-sleep 30
-
-### ~~~~~~~~~~ ###
-for variable in VH VV
-do
-    for year in 2017 2018 2019
+if [ `uname` != Darwin ]
+then
+    ### ~~~~~~~~~~ ###
+    myRscript=${codeDIR}/main-02.R
+    stdoutFile=${outputDIR}/stdout.R.`basename ${myRscript} .R`
+    stderrFile=${outputDIR}/stderr.R.`basename ${myRscript} .R`
+    R --no-save --args ${dataDIR} ${codeDIR} ${pkgDIR} ${outputDIR} < ${myRscript} > ${stdoutFile} 2> ${stderrFile}
+    sleep 30
+    ### ~~~~~~~~~~ ###
+    for variable in VH VV
     do
-        myRscript=${codeDIR}/main-03.R
-        stdoutFile=${outputDIR}/stdout.R.`basename ${myRscript} .R`-${variable}-${year}
-        stderrFile=${outputDIR}/stderr.R.`basename ${myRscript} .R`-${variable}-${year}
-        R --no-save --args ${dataDIR} ${codeDIR} ${pkgDIR} ${outputDIR} ${variable} ${year} < ${myRscript} > ${stdoutFile} 2> ${stderrFile}
-        sleep 30
+        for year in 2017 2018 2019
+        do
+            myRscript=${codeDIR}/main-03.R
+            stdoutFile=${outputDIR}/stdout.R.`basename ${myRscript} .R`-${variable}-${year}
+            stderrFile=${outputDIR}/stderr.R.`basename ${myRscript} .R`-${variable}-${year}
+            R --no-save --args ${dataDIR} ${codeDIR} ${pkgDIR} ${outputDIR} ${variable} ${year} < ${myRscript} > ${stdoutFile} 2> ${stderrFile}
+            sleep 30
+        done
     done
-done
+    ### ~~~~~~~~~~ ###
+fi
 
 ##################################################
 exit 0
