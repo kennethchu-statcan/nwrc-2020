f9221e386e47b7515700446393b7d1229700d416
Kenneth Chu <kennethchu@Kenneths-MacBook-Pro.local>
2020-12-30 06:20:09 -0500

    updated: run-main.sh

--- 401-manuscript/run-main.sh
+++ 401-manuscript/run-main.sh
@@ -20,7 +20,7 @@ cp    $0         ${outputDIR}/code
 myRscript=${codeDIR}/main-01.R
 stdoutFile=${outputDIR}/stdout.R.`basename ${myRscript} .R`
 stderrFile=${outputDIR}/stderr.R.`basename ${myRscript} .R`
-# R --no-save --args ${dataDIR} ${codeDIR} ${pkgDIR} ${outputDIR} < ${myRscript} > ${stdoutFile} 2> ${stderrFile}
+R --no-save --args ${dataDIR} ${codeDIR} ${pkgDIR} ${outputDIR} < ${myRscript} > ${stdoutFile} 2> ${stderrFile}
 
 ### ~~~~~~~~~~ ###
 for variable in VH VV
